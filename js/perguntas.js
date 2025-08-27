cc24b3d3a607d598eac900606ee3da607487f750
Felipe-407 <ribeiro.andrade.felipe@escola.pr.gov.br>
2025-08-27 07:43:51 -0300

    .

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,1 +1,97 @@
-
+const perguntas = [
+    {
+        enunciado: "pergunta 1",
+        alternativas: [
+            {
+                texto: "afirmativa 1",
+                afirmacao: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            },
+           {
+                texto: "afirmativa 2",
+                afirmacao: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            }
+        ]
+    },
+    {
+        enunciado: "pergunta 1",
+        alternativas: [
+            {
+                texto: "afirmativa 1",
+                afirmacao: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            },
+           {
+                texto: "afirmativa 2",
+                afirmacao: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            }
+        ]
+    },
+    {
+        enunciado: "pergunta 1",
+        alternativas: [
+            {
+                texto: "afirmativa 1",
+                afirmacao: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            },
+           {
+                texto: "afirmativa 2",
+                afirmacao: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            }
+        ]
+    },
+    {
+        enunciado: "pergunta 1",
+        alternativas: [
+            {
+                texto: "afirmativa 1",
+                afirmacao: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            },
+           {
+                texto: "afirmativa 2",
+                afirmacao: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            }
+        ]
+    },
+    {
+        enunciado: "pergunta 1",
+        alternativas: [
+            {
+                texto: "afirmativa 1",
+                afirmacao: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            },
+           {
+                texto: "afirmativa 2",
+                afirmacao: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            }
+        ]
+    }
+ ]
